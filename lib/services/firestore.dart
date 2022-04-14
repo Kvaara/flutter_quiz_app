aa6904687548878bf21d3d8be9afa1b7e6bb22f0
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-04-14 13:03:29 +0300

    Implemented a streamReport method that can be used to fetch report data associated to the currently logged in user. This is a realtime stream, which means that it listens to its associated data's changes. I installed the RxDart package so that using Streams is much easier and enjoyable.

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,6 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:flutter_quiz_app/models/quiz.dart';
+import 'package:flutter_quiz_app/models/report.dart';
 import 'package:flutter_quiz_app/models/topic.dart';
+import 'package:flutter_quiz_app/services/auth.dart';
+import 'package:rxdart/rxdart.dart';
 
 class Firestore {
   final _firestoreDB = FirebaseFirestore.instance;
@@ -20,5 +24,19 @@ class Firestore {
     final quizJSON = quizSnapshot.data() ?? {};
     final quiz = Quiz.fromJson(quizJSON);
     return quiz;
+  }
+
+  Stream<Report> streamReport() {
+    return Auth.userStream.switchMap((user) {
+      if (user != null) {
+        final ref = _firestoreDB.collection("reports");
+        final userReportsStream = ref.doc(user.uid).snapshots();
+        return userReportsStream
+            .map((reportJSON) => Report.fromJson(reportJSON.data() ?? {}));
+      }
+      print(
+          "User is not logged in... Report Stream consists of only mockup Reports");
+      return Stream.fromIterable([Report()]);
+    });
   }
 }
