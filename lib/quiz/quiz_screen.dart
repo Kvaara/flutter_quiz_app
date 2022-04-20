d983bbcf90661f29e7238e6d8070e75364122bcc
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-04-20 22:29:49 +0300

    There is now both a congratulations and questio page. Question page has the question related to the quiz and the congratulations screen is shown after the user has completed all of the questions.

--- lib/quiz/quiz_screen.dart
+++ lib/quiz/quiz_screen.dart
@@ -5,6 +5,8 @@ import 'package:flutter_quiz_app/shared/loading_indicator.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
+import '../models/option.dart';
+import '../models/question.dart';
 import '../services/firestore.dart';
 import '../shared/progress_bar.dart';
 
@@ -57,6 +59,140 @@ class QuizScreen extends StatelessWidget {
           }
         },
       ),
+    );
+  }
+}
+
+class StartPage extends StatelessWidget {
+  final Quiz quiz;
+  const StartPage({Key? key, required this.quiz}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var state = Provider.of<QuizState>(context);
+
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(quiz.title, style: Theme.of(context).textTheme.headline4),
+          const Divider(),
+          Expanded(child: Text(quiz.description)),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton.icon(
+                onPressed: state.nextPage,
+                label: const Text('Start Quiz!'),
+                icon: const Icon(Icons.poll),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CongratsPage extends StatelessWidget {
+  final Quiz quiz;
+  const CongratsPage({Key? key, required this.quiz}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Congrats! You completed the ${quiz.title} quiz',
+            textAlign: TextAlign.center,
+          ),
+          const Divider(),
+          Image.asset('assets/congrats.gif'),
+          const Divider(),
+          ElevatedButton.icon(
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.green,
+            ),
+            icon: const Icon(FontAwesomeIcons.check),
+            label: const Text(' Mark Complete!'),
+            onPressed: () {
+              Firestore().updateUserReport(quiz);
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                '/topics',
+                (route) => false,
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class QuestionPage extends StatelessWidget {
+  final Question question;
+  const QuestionPage({Key? key, required this.question}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var state = Provider.of<QuizState>(context);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+            child: Text(question.text),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: question.options.map((opt) {
+              return Container(
+                height: 90,
+                margin: const EdgeInsets.only(bottom: 10),
+                color: Colors.black26,
+                child: InkWell(
+                  onTap: () {
+                    state.selected = opt;
+                    // _bottomSheet(context, opt, state);
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    child: Row(
+                      children: [
+                        Icon(
+                            state.selected == opt
+                                ? FontAwesomeIcons.circleCheck
+                                : FontAwesomeIcons.circle,
+                            size: 30),
+                        Expanded(
+                          child: Container(
+                            margin: const EdgeInsets.only(left: 16),
+                            child: Text(
+                              opt.value,
+                              style: Theme.of(context).textTheme.bodyText2,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+        )
+      ],
     );
   }
 }
